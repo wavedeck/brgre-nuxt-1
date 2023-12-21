200c27829702dd6c7bf1c4a5e47b495fd1c563a0
Invadox <f.erol@invadox.at>
2023-12-21 21:12:04 +0100

    snapshot 2023-12-21

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,390 @@
 <template>
   <main id="main-content">
-    <PageIndexHero/>
+    <section id="index-hero" class="relative">
+    <div class="hero-header">
+      <div class="hero-header__content container mx-auto">
+        <h1
+          class="relative p-4 before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-hero-opaque md:max-w-xl md:before:-left-1/2 md:before:w-[150%] lg:max-w-5xl"
+        >
+          <span
+            class="relative z-10 font-display text-2xl uppercase text-primary lg:text-3xl xl:text-4xl"
+          >
+            Wo Innovation und Tradition in der Schweisstechnik
+            aufeinandertreffen
+          </span>
+        </h1>
+      </div>
+    </div>
+
+    <div class="hero-description">
+        <div class="hero-description__button container mx-auto flex gap-2">
+          <img class="hero-description__arrow" src="/img/arrows.webp" alt="" aria-label="hidden">
+        <a
+          href="#"
+          class="bregtec-button--dark flex-1 bg-secondary p-4 text-center uppercase text-white"
+          >Schweisstechnik</a
+        >
+        <a
+          href="#"
+          class="bregtec-button--dark flex-1 bg-secondary p-4 text-center uppercase text-white"
+          >Projektmanagement & Gesamtlösungen</a
+        >
+        <a
+          href="#"
+          class="bregtec-button--dark flex-1 bg-secondary p-4 text-center uppercase text-white"
+          >Services</a
+        >
+      </div>
+      <p class="mx-auto max-w-4xl lg:mt-8">
+        Mit über 20 Jahren Erfahrung im anspruchsvollen Stahl-, Anlagen-,
+        Apparate- und Rohrleitungsbau sind wir im St. Galler Rheintal ansässig
+        und als offizieller Partner von Böhler Welding Ihr erster
+        Ansprechpartner für Komplettlösungen in der Schweisstechnik. Die
+        perfekte Abstimmung von Schweissgeräten, Schweisszusatzwerkstoffen und
+        modernsten Technologien in Kombination mit unserem renommierten
+        Anwendungs- und Prozess-Know-how bietet die beste Lösung für Ihre
+        Anforderungen.
+      </p>
+    </div>
+  </section>
+    <section id="index-intro">
+      <div>
+        <h2
+          class="my-8 text-center font-display text-2xl uppercase text-secondary"
+        >
+          Willkommen in einer Welt, in der Schweisstechnik und Projektmanagement
+          zu einer Einheit verbunden werden.
+        </h2>
+        <hr class="mb-8 text-secondary" />
+        <div class="index-intro__wrapper">
+          <div class="index-intro__card">
+            <img
+              src="/img_unoptimized/boe_wire1.png"
+              alt=""
+              class="intro-card__img h-80"
+            />
+            <div
+              class="index-intro__content bg-secondary"
+            >
+            <div class="intro__content__inner ">
+              <h3
+                class="mb-4 text-center font-display text-2xl uppercase text-white"
+              >
+                Alle Produkte
+              </h3>
+              <p
+                class="max-w-[35ch] text-center font-display uppercase text-white"
+              >
+                Qualität, die keine Kompromisse kennt offizieller voestalpine
+                Böhler Händler
+              </p>
+              <p class="mb-8 mt-16">
+                Als offizieller Händler von Böhler Welding im St. Galler
+                Rheintal und der Ostschweiz setzen wir die neuesten Innovationen
+                in der Schweisstechnik für Ihren Erfolg ein. Kaufen oder mieten
+                Sie komplette Schweissgeräte und entdecken Sie ab Februar 2024
+                unser breites Sortiment in unserem Online-Shop.
+              </p>
+              </div>
+              <img src="/img/Frame.png" alt="" class="mb-16"/>
+              <a href="#" class="bregtec-button my-12">Zu unseren Produkten</a>
+            </div>
+          </div>
+
+          <div class="index-intro__card">
+            <img
+              src="/img_unoptimized/boe_wire2.png"
+              alt=""
+              class="h-80 w-full object-cover"
+            />
+            <div
+              class="index-intro__content bg-secondary"
+            >
+            <div class="intro__content__inner ">
+              <h3
+                class="mb-4 text-center font-display text-2xl uppercase text-white"
+              >
+                Projektmanagement & Gesamtlösungen
+              </h3>
+              <p
+                class="mb-16 max-w-[35ch] text-center font-display uppercase text-white"
+              >
+                Your project is our priority! Umfassende Lösungen für Ihr
+                Projektgeschäft
+              </p>
+              <p class="mb-16">
+                Wir bieten Ihnen Schweissversuche, Auftrags- und
+                Reparaturschweissungen, Prototypen- und Kleinserienfertigungen
+                über gesamte Projektleitungen von der Materialbeschaffung bis
+                hin zur Montage inklusive zerstörungsfreier Prüfungen, der
+                Qualitätsdokumentation und noch vieles mehr an - alles, was Sie
+                für Ihr Projekt benötigen. Sie bekommen alles aus einer Hand!
+              </p>
+              </div>
+              <a href="#" class="bregtec-button my-12"
+                >Zu unseren Dienstleistungen
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="index-precisiion__container">
+      <div
+        class="index-precision__wrapper container bg-cover bg-center"
+        style="background-image: url('/img_unoptimized/rect2.png');"
+      >
+        <div
+          class=""
+        >
+          <h1
+            class="relative p-4 before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-hero-opaque md:max-w-xl md:before:-left-1/2 md:before:w-[150%] lg:max-w-5xl"
+          >
+            <span
+              class="relative z-10 font-display text-xl uppercase text-primary lg:text-3xl lg:leading-normal xl:text-4xl xl:leading-relaxed"
+            >
+              Präzision ohne Kompromisse <br> für die Elite der Schweisser:innen
+            </span>
+          </h1>
+        </div>
+      </div>
+    </section>
+
+    <section id="schweiss-bedarf">
+      <div class="scheiss-bedarf__wrapper container mx-auto">
+        <div class="mt-12 flex flex-col md:flex-row lg:mt-24">
+          <img src="/img_unoptimized/rect3.png" alt="" class="object-cover md:w-2/5" />
+          <div class="bg-secondary px-4 py-6 lg:px-10 lg:py-8 flex flex-col justify-center">
+            <h2 class="font-display text-lg uppercase text-white">
+              Sie haben einen schweisstechnischen Bedarf?
+            </h2>
+            <p class="mt-4">
+              Was auch immer Sie schweissen möchten, Sie können sich auf die
+              hochwertigen Schweisszusatzwerkstoffe der Marken Böhler Welding
+              und UTP verlassen. Diese Produkte werden sowohl für Standard- als
+              auch für Spezialanwendungen hergestellt und erfüllen die höchsten
+              Qualitätsstandards.
+            </p>
+          </div>
+        </div>
+
+        <div class="my-24">
+          <div class="flex gap-2 scheiss-tab">
+            <button class="bg-secondary px-8 py-4 uppercase text-white"
+              >Equipment</button
+            >
+            <button class="bg-primary px-8 py-4 uppercase text-white"
+              >Schweisszusätze</button
+            >
+            <button class="bg-secondary px-8 py-4 uppercase text-white"
+              >Persönliche Schutzausrüstung</button
+            >
+            <button class="bg-secondary px-8 py-4 uppercase text-white"
+              >Schweisszubehör</button
+            >
+          </div>
+          <div>
+            <div class="schweiss-tab__content bg-primary p-16 text-white">
+              <div class="grid w-1/4 place-items-center">
+                <img src="/img/Icon.svg" alt="" />
+              </div>
+              <div class="schweiss-tab__list px-4 xl:w-1/4">
+                <ul class="list-disc font-display uppercase">
+                  <li class="py-2">Stabelektroden</li>
+                  <li class="py-2">WIG Stäbe</li>
+                  <li class="py-2">Massivdrähte</li>
+                  <li class="py-2">Gasgeschützte Fülldrähte</li>
+                  <li class="py-2">Selbstschützende Fülldrähte</li>
+                </ul>
+              </div>
+              <div class="schweiss-tab__list px-4 xl:w-1/2">
+                <ul class="list-disc font-display uppercase">
+                  <li class="py-2">UP-Drahtelektroden und Schweisspulver</li>
+                  <li class="py-2">Bandelektroden</li>
+                  <li class="py-2">Metallpulver</li>
+                  <li class="py-2">Fülldrähte zum Lichtbogenspritzen</li>
+                  <li class="py-2">Keramische Schweissbadsicherung</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="">
+      <h2
+        class="mb-6 text-center font-display text-3xl uppercase text-secondary"
+      >
+        Unser Service für Sie
+      </h2>
+      <div class="bg-primary px-8 py-8 text-white lg:py-12">
+        <p class="mx-auto mb-16 max-w-4xl font-display uppercase">
+          Mit unserem massgescheiderten Service und unseren Dienstleistungen
+          möchten wir Ihren Arbeitsalltag erleichtern und Sie dabei
+          unterstützen, Ihre Projekte erfolgreich abzuschließsen.
+        </p>
+      </div>
+
+      <div class="service-card__wrapper container mx-auto max-w-5xl -mt-16 flex flex-wrap">
+        <div class="service-card">
+          <div class="overflow-hidden">
+            <img
+              src="/img_unoptimized/4.png"
+              alt=""
+              class="h-full w-full object-cover transition-transform duration-500 hover:scale-125"
+            />
+          </div>
+          <a
+            href="#"
+            class="bregtec-button mt-2 w-full rounded-none py-7 text-left"
+            >Show- und Demo Room</a
+          >
+        </div>
+        <div class="service-card">
+          <div class="overflow-hidden">
+            <img
+              src="/img_unoptimized/9.png"
+              alt=""
+              class="h-full w-full object-cover transition-transform duration-500 hover:scale-125"
+            />
+          </div>
+          <a
+            href="#"
+            class="bregtec-button mt-2 w-full rounded-none py-7 text-left"
+            >Montage & Inbetriebnahme
+          </a>
+        </div><div class="service-card">
+          <div class="overflow-hidden">
+            <img
+              src="/img_unoptimized/10.png"
+              alt=""
+              class="h-full w-full object-cover transition-transform duration-500 hover:scale-125"
+            />
+          </div>
+          <a
+            href="#"
+            class="bregtec-button mt-2 w-full rounded-none py-7 text-left"
+            >Testschweissungen</a
+          >
+        </div><div class="service-card">
+          <div class="overflow-hidden">
+            <img
+              src="/img_unoptimized/11.png"
+              alt=""
+              class="h-full w-full object-cover transition-transform duration-500 hover:scale-125"
+            />
+          </div>
+          <a
+            href="#"
+            class="bregtec-button mt-2 w-full rounded-none py-7 text-left"
+            >Maschinenvorführungen
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <section class="mt-8">
+      <div class="container mx-auto">
+        <h2
+          class="mb-6 text-center font-display text-3xl uppercase text-secondary"
+        >
+          Unsere Dienstleistungen
+        </h2>
+        <hr class="mb-8 text-secondary" />
+        <div class="index-service__wrapper mt-8 flex flex-col md:flex-row-reverse lg:mt-16">
+          <img src="/img_unoptimized/12.png" alt="" class="service-img" />
+          <div
+            class="grid place-items-center bg-secondary px-4 py-6 lg:px-10 lg:py-8 h-full"
+          >
+            <h2 class="font-display text-lg uppercase text-white">
+              Sie suchen einen qualitativ hochwertigen Schweisspartner,
+              zuverlässige Outsourcing-Lösungen oder umfassende Dienstleistungen
+              im Bereich der Schweisstechnik? Bei Bregtec sind Sie hier genau
+              richtig! Dank unseres weitreichenden Netzwerks bieten wir Ihnen
+              Schweisßdienstleistungen aus einer Hand, sodass Sie sämtliche
+              Leistungen komfortabel und effizient direkt über uns beziehen
+              können.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-32">
+      <div
+        class="hand-background bg-cover bg-top"
+        style="background-image: url('/img_unoptimized/13.png');"
+      >
+        <div
+          class="hand-text-container"
+        >
+          <h1
+            class="relative p-4 before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-hero-opaque md:max-w-xl md:before:-left-1/2 md:before:w-[150%] lg:max-w-5xl"
+          >
+            <span
+              class="relative z-10 font-display text-2xl uppercase text-white lg:text-3xl lg:leading-normal xl:text-4xl xl:leading-relaxed"
+            >
+              Hand in Hand mit den Besten
+            </span>
+          </h1>
+        </div>
+      </div>
+    </section>
+    <section class="mb-16">
+      <div class="container mx-auto">
+        <div class="mt-16 flex flex-col items-center justify-center md:flex-row md:items-end">
+          <div class="w-1/2 flex justify-center">
+            <img src="/img/bregtec_logo_color.svg" class="h-20" alt="" />
+          </div>
+          <div class="w-1/2 flex justify-center">
+            <img src="/img_unoptimized/16.png" class="h-36" alt="" />
+          </div>
+        </div>
+        <div class="mt-16 px-12 flex flex-col items-center justify-center items-stretch lg:flex-row">
+          <div class="w-full lg:w-1/2">
+            <img src="/img_unoptimized/17.png" alt="" class="w-full h-full object-cover" />
+          </div>
+
+          <div class="w-full lg:w-1/2 bg-primary text-white p-12">
+            <h2 class="font-display text-2xl uppercase text-secondary mb-8">
+              Dafür stehen wir:
+            </h2>
+            <p class="mb-8 font-display uppercase">
+              Unser Ziel ist es, Sie bei der Auswahl des richtigen
+              Schweisszusatzwerkstoffes sowie bei der optimalen Schweiss- und
+              Schneidanlagen zu unterstützen und die Gestaltung Ihrer
+              Fertigungsprozesse zu optimieren. Bei Bregtec stehen Sie im
+              Mittelpunkt, und wir sind darauf spezialisiert, massgeschneiderte
+              Lösungen für Ihre individuellen Bedürfnisse auszuarbeiten.
+            </p>
+            <ul class="mb-8 list-disc pl-8">
+              <li>
+                Welcher Schweisszusatz ist für meine Aufgabenstellung am besten
+                geeignet, um die mechanischen Werte zu erreichen?
+              </li>
+              <li class="my-4">
+                Mit welcher Konfiguration kann ich die Abschmelzleistung sowie
+                die Qualität in meinem Fertigungsprozess steigern?
+              </li>
+              <li>
+                Welche Stromquelle ist mit meinen bestehenden Brennern
+                kompatibel?
+              </li>
+            </ul>
+            <p class="my-8 font-display uppercase">
+              Kontaktieren Sie uns noch heute, um mit einem zuverlässigen und
+              zukunftsorientierten Partner in der Welt der Schweisstechnik
+              zusammenzuarbeiten!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
+
+<style lang="scss" scoped>
+  @use "~/assets/scss/pages/index";
+</style>
 
 <script setup lang="ts"></script>
