29aaaa7e962cacd4eb26f8bcba902e60f004cf7d
wavedeck <david@invadox.at>
2023-12-21 20:09:32 +0100

    add dienstleistungen page

--- pages/dienstleistungen.vue
+++ pages/dienstleistungen.vue
@@ -1,1 +1,121 @@
-<template></template>
+<template>
+  <main id="main-content">
+    <section id="hero" class="relative">
+      <div
+        class="bg-cover bg-center"
+        style="
+          background-image: url(&quot;/img/dienstleistungen/dienstleistungen_hero.webp&quot;);
+        "
+      >
+        <div
+          class="container mx-auto p-6 md:pb-16 md:pt-40 lg:pb-24 lg:pt-40 xl:pt-64"
+        >
+          <h1
+            class="relative px-4 py-8 before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-hero-opaque md:max-w-xl md:before:-left-1/2 md:before:w-[150%] lg:max-w-5xl"
+          >
+            <span
+              class="relative z-10 font-display text-2xl uppercase text-primary lg:text-3xl xl:text-4xl"
+            >
+              Entdecken Sie erstklassige Gesamtlösungen in der Schweisstechnik
+              und professionelles Projektmanagement
+            </span>
+          </h1>
+        </div>
+      </div>
+
+      <div class="bg-primary px-8 py-8 text-white lg:py-12">
+        <p class="mx-auto max-w-4xl">
+          Wir kennen die hohen Anforderungen auf dem Schweizer Markt und sind
+          Ihr zuverlässiger Partner für technisch anspruchsvolle
+          Schweisstechnik. Unser breites Verfahrensangebot, unser grosses
+          Netzwerk und unsere kompetente Beratung ermöglichen Lösungen für
+          Projekte in nahezu jeder Grösse und Branche.
+        </p>
+      </div>
+    </section>
+
+    <section id="dienstleistungen">
+      <div class="container mx-auto px-4">
+        <div class="mt-12 flex flex-col md:flex-row lg:mt-24">
+          <img
+            src="/img/dienstleistungen/kelag_schweissnaht_rohre.webp"
+            alt=""
+            class="object-cover md:w-2/5"
+          />
+          <div
+            class="flex flex-col justify-center bg-secondary px-4 py-6 lg:px-10 lg:py-8"
+          >
+            <h2 class="font-display text-lg uppercase text-white lg:text-4xl">
+              Ihr Schweisser ist ausgefallen oder Sie verfügen temporär nicht
+              über ausreichend Ressourcen?
+            </h2>
+            <p class="mt-4">
+              Wenn Sie das Schweissen auslagern möchten, sind wir der kompetente
+              Partner, den Sie suchen! Mit unserer langjährigen Erfahrung in
+              verschiedenen metallverarbeitenden Branchen bieten wir
+              erstklassige Qualität und massgeschneiderte Lösungen für Ihre
+              Bedürfnisse. Vom Anlagen- und Rohrleitungsbau bis hin zur Pharma-,
+              Chemie-, Vakuumtechnik, Lebensmittelindustrie und dem
+              Kraftwerksbau - wir sind Ihr zuverlässiger Ansprechpartner!
+            </p>
+          </div>
+        </div>
+
+        <div class="mt-6 flex flex-col md:flex-row lg:mt-16 xl:mt-24">
+          <img
+            src="/img/dienstleistungen/kelag_machine_technical.webp"
+            alt=""
+            class="object-cover md:w-2/5"
+          />
+          <div
+            class="flex flex-col justify-center bg-primary px-4 py-6 lg:px-10 lg:py-8"
+          >
+            <h2 class="font-display text-lg uppercase text-white lg:text-4xl">
+              Damit alles richtig zusammengeführt wird!
+            </h2>
+            <p class="my-4 font-display uppercase text-white lg:text-xl">
+              Nur einen Ansprechpartner zu haben, bedeutet für unsere Kunden
+              weniger Schnittstellen
+            </p>
+            <p class="font-display text-lg uppercase text-white lg:text-3xl">
+              Projektmanagement und mehr ...
+            </p>
+            <p class="mt-4 text-white">
+              Wir unterstützen Sie nicht nur bei der Auslagerung von
+              Schweissarbeiten, sondern bieten Ihnen auch ein umfassendes
+              Projektmanagement. Unsere Partnerschaften mit zuverlässigen
+              Unternehmen ermöglichen es uns, mechanische Bearbeitungen,
+              Oberflächenbehandlungen, Prototypenbauten und vieles mehr zu
+              organisieren und zu vermitteln.
+            </p>
+          </div>
+        </div>
+
+        <div class="my-16 lg:my-24">
+          <h2
+            class="mb-8 text-center font-display text-lg uppercase text-primary lg:text-4xl"
+          >
+            Unser Leistungsversprechen - mit Handschlag-Qualität
+          </h2>
+
+          <p class="text-center">
+            Unser Hauptziel ist die Zufriedenheit unserer Kunden. Wir bieten
+            innovative, wirtschaftliche und zuverlässige Lösungen in enger
+            Zusammenarbeit mit Ihnen. Von der Idee über Prototypen bis hin zur
+            Auslieferung der fertigen Anlage sind wir Ihr Partner für schnelle
+            und qualitativ hochwertige Ergebnisse.
+          </p>
+          <p class="mt-4 text-center">
+            Kontaktieren Sie uns noch heute, um herauszufinden, wie wir Ihnen
+            bei Ihren Projekten und Ihren individuellen Anforderungen helfen
+            können.
+          </p>
+
+          <a href="#" class="bregtec-button mx-auto mt-8 lg:mt-16"
+            >Jetzt anfragen</a
+          >
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
