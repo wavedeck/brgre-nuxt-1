e2af115d589344593e93ead78979600dfcf87e47
wavedeck <david@invadox.at>
2023-12-21 20:09:17 +0100

    add style for contact / company

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,1 +1,198 @@
-<template></template>
+<template>
+  <section id="kontakt-hero" class="relative">
+    <div class="hero-header">
+      <div class="hero-header__content container mx-auto">
+        <h1 class="hero-heading">
+          <span class="hero-heading__inner">Was dürfen wir für Sie tun?</span>
+        </h1>
+      </div>
+    </div>
+
+    <div class="hero-description"></div>
+  </section>
+
+  <section id="kontakt-form" class="my-12">
+    <div class="kontakt-content__wrapper container mx-auto px-4 lg:flex gap-4">
+      <div class="kontakt-content__col1">
+        <h2 class="kontakt-content__heading">Kontaktinformationen</h2>
+        <div class="flex items-center gap-4">
+          <font-awesome-icon icon="fas fa-phone text-white" />
+          <a href="tel:+41817561530" class="font-display uppercase text-white"
+            >+41 81 756 15 30</a
+          >
+        </div>
+        <div class="my-2 flex items-center gap-4">
+          <font-awesome-icon icon="fas fa-envelope text-white" />
+          <a
+            href="mailto:info@bregtec.com"
+            class="font-display uppercase text-white underline"
+            >info@bregtec.com</a
+          >
+        </div>
+        <img src="/img/bregtec-company-sm.webp" alt="" />
+        <div class="mt-4 flex items-start gap-4">
+          <font-awesome-icon icon="fas fa-location-dot text-white" />
+          <p class="font-display uppercase text-white">
+            Bregtec GmbH<br /><br />
+            Langäulistrasse 9<br />
+            9470 Buchs, Schweiz<br /><br />
+            Sie finden uns im 4. Obergeschoss.
+          </p>
+        </div>
+      </div>
+      <div class="kontakt-content__col2">
+        <form action="/api/form/contactform" method="post">
+          <div class="gap-4 md:flex">
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-gender" class="mb-2 block font-display"
+                >Anrede</label
+              >
+              <select
+                name="gender"
+                id="contactform-gender"
+                class="w-full border-b pb-2"
+              >
+                <option value="n" selected disabled>Bitte auswählen</option>
+                <option value="m">Herr</option>
+                <option value="f">Frau</option>
+                <option value="o">Divers</option>
+              </select>
+            </div>
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-company" class="mb-2 block font-display"
+                >Firma</label
+              >
+              <input
+                type="text"
+                name="company"
+                id="contactform-company"
+                class="w-full border-b pb-2"
+              />
+            </div>
+          </div>
+          <div class="gap-4 md:flex">
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-lastname" class="mb-2 block font-display"
+                >Name</label
+              >
+              <input
+                type="text"
+                name="lastname"
+                id="contactform-lastname"
+                class="w-full border-b pb-2"
+              />
+            </div>
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-firstname" class="mb-2 block font-display"
+                >Vorname</label
+              >
+              <input
+                type="text"
+                name="firstname"
+                id="contactform-firstname"
+                class="w-full border-b pb-2"
+              />
+            </div>
+          </div>
+          <div class="gap-4 md:flex">
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-email" class="mb-2 block font-display"
+                >E-Mail</label
+              >
+              <input
+                type="email"
+                name="email"
+                id="contactform-email"
+                class="w-full border-b pb-2"
+              />
+            </div>
+            <div class="mb-4 md:w-1/2">
+              <label for="contactform-phone" class="mb-2 block font-display"
+                >Telefon</label
+              >
+              <input
+                type="tel"
+                name="phone"
+                id="contactform-phone"
+                class="w-full border-b pb-2"
+              />
+            </div>
+          </div>
+
+          <div class="mb-4">
+            <label for="contactform-regards" class="mb-2 block font-display"
+              >Betreff</label
+            >
+            <select
+              name="regards"
+              id="contactform-regards"
+              class="w-full border-b pb-2"
+            >
+              <option>Projektanfrage</option>
+              <option>Terminvereinbarung Beratung und Verkauf</option>
+              <option>Produktvorführung</option>
+              <option>Gerätevermietung</option>
+              <option>Service/Dienstleistung</option>
+              <option>Onlineshop</option>
+              <option>Sonstiges</option>
+            </select>
+          </div>
+
+          <div>
+            <label for="contactform-message" class="mb-2 block font-display"
+              >Nachricht</label
+            >
+            <textarea
+              name="message"
+              id="contactform-message"
+              class="w-full bg-[hsl(0,_0%,_85%)]"
+              rows="8"
+            ></textarea>
+          </div>
+
+          <button class="bregtec-button my-8 ml-auto" type="submit">Senden</button>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use "~/assets/scss/pages/kontakt";
+
+.kontakt-content__wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+.kontakt-content__col1 {
+  color: #fff;
+  background-color: #ada48a;
+  padding: 2rem;
+
+  .kontakt-content__heading {
+    text-transform: uppercase;
+    font-family: "Exo", system-ui, sans-serif;
+    font-size: 1.25rem;
+    margin-bottom: 1.5rem;
+  }
+}
+
+// MEDIA
+
+@media screen and (min-width: 1024px) {
+
+  .kontakt-content__wrapper {
+    flex-direction: row;
+}
+
+  .kontakt-content__col1 {
+    width: calc(36% - 2rem);
+  }
+
+  .kontakt-content__col2 {
+    width: calc(64% - 2rem);
+  }
+}
+</style>
--- pages/unternehmen.vue
+++ pages/unternehmen.vue
@@ -1,1 +1,79 @@
-<template></template>
+<template>
+  <section id="unternehmen-hero" class="relative">
+    <div class="hero-header">
+      <div class="hero-header__content container mx-auto">
+        <h1 class="hero-heading">
+          <span class="hero-heading__inner"> Bregtec - Das sind wir </span>
+        </h1>
+      </div>
+    </div>
+
+    <div class="hero-description"></div>
+  </section>
+
+  <section id="unternehmen-content">
+    <div class="container mx-auto px-4">
+      <h2 class="unternehmen-heading-main">
+        Bregtec - Ihr Erfolgspartner in der Welt der Schweisstechnik!
+      </h2>
+      <div class="unternehmen-content__wrapper">
+        <div class="unternehmen-content-col1">
+          <img
+            class="patrick-portrait-circle"
+            src="/img/patrick-bregant-portrait-white.webp"
+            alt=""
+          />
+          <p class="text-sm">
+            “Bei Bregtec ist Schweissen nicht nur ein Prozess, sondern bei uns
+            geht die Schweisstechnik mit all ihren faszinierenden Facetten weit
+            über das Funktionale hinaus und es ist jeden Tag aufs Neue wieder
+            beeindruckend zu sehen, wie die perfekte Abstimmung zwischen Mensch,
+            Maschine und Erfahrung einen massgeblichen Beitrag zur Errichtung
+            des Fundaments unserer modernen Welt beitragen kann.”
+          </p>
+          <p class="mt-8 text-lg">Patrick Bregant</p>
+          <p class="mb-24 flex-1">Inhaber & Geschäftsführer</p>
+
+          <img src="/img/bregtec_logo_color.svg" alt="" />
+        </div>
+        <div class="unternehmen-content-col2">
+          <p class="unternehmen-content__text mb-8 flex-1">
+            Wir sind Ihr zuverlässiger Partner mit Handschlag-Qualität, der
+            innovative Lösungen für die höchsten Anforderungen des CH- und
+            FL-Marktes bietet. Mit über 20 Jahren Erfahrung streben wir danach,
+            die ideale Lösung für Ihre schweisstechnischen Herausforderungen zu
+            finden. Unsere Expertise, ein breites Verfahrensangebot und ein
+            umfassendes Netzwerk ermöglichen massgeschneiderte Lösungen für
+            schweisstechnische Projekte in nahezu jeder Grösse und Branche.
+            Unser Leistungsangebot erstreckt sich von Beratung, Verkauf und
+            Lieferung von Schweissequipment der Marke «Böhler Welding by
+            voestalpine» über Projektmanagement im Bereich Rohrleitungs-,
+            Anlagen- und Apparatebau bis hin zur schweisstechnischen
+            Unterstützung von Outsourcing-Projekten. Mit einem umfassenden
+            Projektmanagement übernehmen wir die gesamte Materialbeschaffung,
+            organisieren mechanische Bearbeitungen, Oberflächenbehandlungen
+            sowie die Montage durch eingespielte Kooperationen mit unseren
+            vertrauten Lieferanten und Partnern. Unser Hauptziel für Sie ist
+            einfach: Ihr Projekterfolg mit einem Partner und weniger
+            Schnittstellen! Kontaktieren Sie uns noch heute, um zu erfahren, wie
+            wir Ihnen bei Ihren Schweissprojekten und individuellen
+            Anforderungen helfen können. Vertrauen Sie auf BREGTEC - Ihr Partner
+            für betriebswirtschaftlich und qualitativ hochwertige Ergebnisse!
+          </p>
+
+          <a href="/kontakt" class="bregtec-button mb-2 lg:mb-6"
+            >Jetzt Anfragen</a
+          >
+        </div>
+      </div>
+      <p class="unternehmen-referenzen-text">
+        Projektreferenzen und Persönliche Referenzen werden Ihnen gerne auf
+        Anfrage übermittelt.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use "~/assets/scss/pages/unternehmen";
+</style>
